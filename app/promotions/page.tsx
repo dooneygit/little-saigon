a350f667f5649aafec8e8d0c5aecab0d4265095d
DooneyGit <dooneytrue@gmail.com>
2025-05-21 04:36:09 -0400

    responsive promotions page

--- app/promotions/page.tsx
+++ app/promotions/page.tsx
@@ -36,15 +36,15 @@ export default function Promotions() {
         <>
         <NavBar />
         <main className="pt-[13rem] min-h-screen flex flex-col items-center mb-16 animate-[fadeIn_1s_ease-out_forwards]">
-            <div className="relative w-[70%] min-h-screen">
+            <div className="relative w-[100%] lg:w-[70%] min-h-screen">
             <div
               className="absolute inset-0 bg-cover bg-center blur-[4px]"
               style={{ backgroundImage: "url('/images/promotion-deals.avif')" }}
             /> 
               <div className="relative z-10 flex justify-center py-12">
-                <div className="relative bg-rose-50 w-[60%] min-h-screen flex flex-col items-center p-6 rounded-xl shadow-xl">
-                  <h1 className="font-markazi text-pink-900 text-[6rem]">PROMOTIONS</h1>
-                  <ul className="text-[3rem] font-markazi text-pink-300 text-center">
+                <div className="relative bg-rose-50 w-[70%] lg:w-[60%] min-h-screen flex flex-col items-center p-6 rounded-xl shadow-xl">
+                  <h1 className="font-markazi text-pink-900 text-[3rem] sm:text-[4rem] lg:text-[5rem]">PROMOTIONS</h1>
+                  <ul className="text-[2.5rem] lg:text-[3rem] font-markazi text-pink-300 text-center">
                     <li>10% OFF FOR BIRTHDAY PARTIES OR WEDDING GROUPS.</li>
                     <li>10% OFF FOR GROUP OF 4 OR MORE</li>
                   </ul>
